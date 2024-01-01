34b50c355b7de0117181c1f4a44d3ba75fc1e989
yamate11 <yamate11@cent.xii.jp>
2024-01-01 12:18:33 +0900

    offline dynamic connectivity; just skelton

---
+++ clib/offDynConn.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <cassert>
+typedef long long int ll;
+using namespace std;
+
+/*
+  Offline Dynamic Connectivity
+
+  see https://yamate11.github.io/blog/posts/2024/01-01-undo-union-fild/
+
+*/
+
+//////////////////////////////////////////////////////////////////////
+// See help of libins command for dependency spec syntax.
+// @@ !! BEGIN()    ---- UndoUnionFind.cc
+
+// @@ !! END ---- UndoUnionFind.cc
